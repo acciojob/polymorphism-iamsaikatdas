6cd3095ce6116d26f450f2341a9695042cb4a121
Saikat <saikatdas9547@gmail.com>
2023-01-13 19:55:37 +0530

    12 times

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,36 +1,29 @@
 package com.driver;
-
 public class Main {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Product p = new Product();
-
         int firstAnsValue = p.product(2, 3);
-
         int secondAnswerValue = p.product(2, 3, 4);
-
         double thirdAnswerValue = p.product(2.1, 2.9);
-
     }
-}
 
-    class Product {
-        public int product(int x, int y) {
+     public static class Product {
+        public static int product(int x, int y) {
             int ans = x * y;
             return ans;
         }
 
-        public int product(int x, int y, int z) {
-
+        public static int product(int x, int y, int z) {
             int secondAnswer = x * y * z;
             return secondAnswer;
         }
 
-        public double product(double x, double y) {
+        public static double product(double x, double y) {
             double thirdAnswer = x * y;
             return thirdAnswer;
         }
     }
-
+}
 
 //    public static void main(String[] args) {
 //        Product p = new Product();
