71c3845ba4cac9105d0c564be33b07d98231e4b8
Saikat <saikatdas9547@gmail.com>
2023-01-13 09:13:12 +0530

    Soluation-1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-
+    public  static  void  main(String [] args){
+        Product p = new Product();
+        System.out.println(p.product(4, 5));
+    }
+}
+class Product{
+    public int product(int x, int y) {
+        return x + y;
+    }
 }
