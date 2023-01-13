88e1dd89913b6a205b5ffd04f960fb265acb57f2
Saikat <saikatdas9547@gmail.com>
2023-01-13 19:40:47 +0530

    Akdom alag code copy karke kara

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,23 +3,51 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         Product p = new Product();
-        int ansValue = p.product(2, 3);
-        int secondAnsValue = p.product(2, 3, 4);
-        double thirdAnsValue = p.product(2.1, 2.2);
+
+        int ansValue = p.product(2,3);
+
+        int secondAnswerValue=p.product(2,3,4);
+
+        double thirdAnswerValue=p.product(2.1,2.9);
+
     }
 }
 class Product{
-
     public int product(int x, int y) {
-        int ans = x * y;
+        int ans=x*y;
         return ans;
     }
     public int product(int x, int y, int z) {
-        int secondAns = x * y * z;
-        return secondAns;
+
+        int secondAnswer=x*y*z;
+        return  secondAnswer;
     }
+
     public double product(double x, double y) {
-        double thirdAns = x * y;
-        return thirdAns;
+        double thirdAnswer=x*y;
+        return thirdAnswer;
     }
 }
+
+//    public static void main(String[] args) {
+//        Product p = new Product();
+//        int ansValue = p.product(2, 3);
+//        int secondAnsValue = p.product(2, 3, 4);
+//        double thirdAnsValue = p.product(2.1, 2.2);
+//    }
+//}
+//class Product{
+//
+//    public int product(int x, int y) {
+//        int ans = x * y;
+//        return ans;
+//    }
+//    public int product(int x, int y, int z) {
+//        int secondAns = x * y * z;
+//        return secondAns;
+//    }
+//    public double product(double x, double y) {
+//        double thirdAns = x * y;
+//        return thirdAns;
+//    }
+//}
